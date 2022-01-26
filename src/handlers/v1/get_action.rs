bd345f0515556722718425eef683e9676279dde0
Szepesi Tibor <sztibi023@gmail.com>
2022-01-26 22:35:56 +0100

    feat: get_action

---
+++ src/handlers/v1/get_action.rs
@@ -0,0 +1,33 @@
+use crate::entity::actions;
+use actix_web::{http::StatusCode, route, web, Error, HttpResponse};
+use sea_orm::{
+    entity::EntityTrait,
+    query::QueryFilter,
+    DatabaseConnection, ColumnTrait,
+};
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct GetActionRequest {
+    action: String,
+}
+
+#[route("/v1/action/{action}", method = "GET")]
+pub async fn get_action(
+    req: web::Path<GetActionRequest>,
+    db: web::Data<DatabaseConnection>,
+) -> Result<HttpResponse, Error> {
+    let req = req.into_inner();
+
+    let res = actions::Entity::find()
+        .filter(actions::Column::Name.eq(req.action))
+        .one(db.get_ref())
+        .await
+        .unwrap();
+
+    if res.is_some() {
+        Ok(HttpResponse::new(StatusCode::OK))
+    } else {
+        Ok(HttpResponse::new(StatusCode::NOT_FOUND))
+    }
+}
---
+++ tests/get_action.rs
@@ -0,0 +1,37 @@
+mod utils;
+
+use actix_web::{
+    http::StatusCode,
+    test::{call_service, TestRequest},
+};
+use serial_test::serial;
+use serde_json::json;
+
+#[actix_web::test]
+#[serial]
+async fn test_get_action() {
+    let mut app = utils::get_service().await;
+
+    let req = TestRequest::get()
+        .uri("/v1/action/post.create")
+        .to_request();
+
+    assert_eq!(call_service(&mut app, req).await.status(), StatusCode::NOT_FOUND);
+
+    let req = TestRequest::post()
+        .append_header(("Authorization", "valami token"))
+        .uri("/v1/action")
+        .set_json(json!({
+            "action": "post.create",
+            "secure": true,
+        }))
+        .to_request();
+
+    assert_eq!(call_service(&mut app, req).await.status(), StatusCode::OK);
+
+    let req = TestRequest::get()
+        .uri("/v1/action/post.create")
+        .to_request();
+
+    assert_eq!(call_service(&mut app, req).await.status(), StatusCode::OK);
+}

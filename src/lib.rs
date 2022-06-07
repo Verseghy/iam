255da8dc0935d3756177e8cf53a1f3b54dac5e06
Szepesi Tibor <sztibi023@gmail.com>
2022-06-07 03:25:10 +0200

    refactor: move main logic into lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,55 @@
-pub mod auth;
-pub mod database;
+mod auth;
+mod database;
 mod handlers;
 pub mod id;
 mod password;
-pub mod token;
+mod token;
 
 #[cfg(test)]
 pub(crate) mod mock;
 
-pub use database::connect;
-pub use handlers::routes;
+use actix_web::{web::Data, App, HttpServer};
+use handlers::routes;
+use lettre::{
+    transport::smtp::{authentication::Credentials, AsyncSmtpTransport},
+    Tokio1Executor,
+};
+use rand::{rngs::SmallRng, SeedableRng};
+use std::{io, net::SocketAddr};
+
+pub async fn run() -> io::Result<()> {
+    let database = Data::new(database::connect().await);
+    let redis = Data::new(database::connect_redis().await);
+    let smtp_transport = Data::new(create_smtp_transport());
+    let jwt_private = Data::new(token::create_encoding_key());
+    let jwt_public = Data::new(token::create_decoding_key());
+    let rng = Data::new(SmallRng::from_entropy());
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3001));
+    tracing::info!("Listening on port {}", addr.port());
+    HttpServer::new(move || {
+        App::new()
+            .app_data(database.clone())
+            .app_data(redis.clone())
+            .app_data(smtp_transport.clone())
+            .app_data(jwt_private.clone())
+            .app_data(jwt_public.clone())
+            .app_data(rng.clone())
+            .configure(routes)
+    })
+    .bind(addr)?
+    .run()
+    .await
+}
+
+fn create_smtp_transport() -> AsyncSmtpTransport<Tokio1Executor> {
+    AsyncSmtpTransport::<Tokio1Executor>::starttls_relay(
+        &std::env::var("SMTP_HOST").expect("SMTP_HOST not set"),
+    )
+    .unwrap()
+    .credentials(Credentials::new(
+        std::env::var("SMTP_USERNAME").expect("SMTP_USERNAME not set"),
+        std::env::var("SMTP_PASSWORD").expect("SMTP_PASSWORD not set"),
+    ))
+    .build()
+}

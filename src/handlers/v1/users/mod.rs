8055989f83bc02d0a538e98a76c96785caf864b7
Szepesi Tibor <sztibi023@gmail.com>
2022-06-20 03:43:14 +0200

    style: fmt

--- src/handlers/v1/users/mod.rs
+++ src/handlers/v1/users/mod.rs
@@ -6,15 +6,13 @@ use actix_web::web::{self, ServiceConfig};
 use lettre::{AsyncSmtpTransport, Tokio1Executor};
 
 pub fn routes(config: &mut ServiceConfig) {
-    config
-        .service(login::login)
-        .service(
-            web::resource("/invite")
-                .route(web::post().to(invite::invite::<
-                    redis::aio::ConnectionManager,
-                    AsyncSmtpTransport<Tokio1Executor>,
-                    rand::rngs::SmallRng,
-                >))
-                .wrap(permissions!("iam.user.invite")),
-        );
+    config.service(login::login).service(
+        web::resource("/invite")
+            .route(web::post().to(invite::invite::<
+                redis::aio::ConnectionManager,
+                AsyncSmtpTransport<Tokio1Executor>,
+                rand::rngs::SmallRng,
+            >))
+            .wrap(permissions!("iam.user.invite")),
+    );
 }

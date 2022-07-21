771505d709125a3fa9d4547c40b54e772752b938
Szepesi Tibor <sztibi023@gmail.com>
2022-07-21 23:18:48 +0200

    feat: set sqlx logging level

--- src/database.rs
+++ src/database.rs
@@ -1,11 +1,14 @@
 use redis::aio::ConnectionManager;
 use sea_orm::{ConnectOptions, Database, DatabaseConnection};
+use tracing::log::LevelFilter;
 
 pub async fn connect() -> DatabaseConnection {
     tracing::info!("Trying to connect to database");
 
     let url = std::env::var("DATABASE_URL").expect("DATABASE_URL is not set");
-    let opts = ConnectOptions::new(url);
+    let mut opts = ConnectOptions::new(url);
+    opts.sqlx_logging_level(LevelFilter::Debug);
+
     let db = Database::connect(opts).await.unwrap();
 
     tracing::info!("Connected to database");

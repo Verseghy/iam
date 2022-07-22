a8477ceac1d4218b2af60ab2f06f8fbb0baee674
Szepesi Tibor <sztibi023@gmail.com>
2022-07-22 15:48:47 +0200

    fix: remove commented codes

--- src/handlers/v1/mod.rs
+++ src/handlers/v1/mod.rs
@@ -1,14 +1,5 @@
 mod actions;
 mod users;
-//
-// use actix_web::web;
-// use actix_web::web::ServiceConfig;
-//
-// pub fn routes(config: &mut ServiceConfig) {
-//     config
-//         .service(web::scope("/users").configure(users::routes))
-//         .configure(actions::routes);
-// }
 
 use axum::Router;
 
@@ -16,6 +7,4 @@ pub fn routes() -> Router {
     Router::new()
         .merge(actions::routes())
         .nest("/users", users::routes())
-    // .nest("/action", actions::action_routes())
-    // .nest("/actions", actions::actions_routes())
 }

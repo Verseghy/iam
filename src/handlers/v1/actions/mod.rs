7eeea096ef0ef77fe4fd3e6c84141ec337a8bbef
Szepesi Tibor <sztibi023@gmail.com>
2022-07-27 01:17:56 +0200

    refactor: use /actions everywhere

--- src/handlers/v1/actions/mod.rs
+++ src/handlers/v1/actions/mod.rs
@@ -13,24 +13,16 @@ use axum::{
 
 pub fn routes() -> Router {
     Router::new()
-        .nest("/action", {
-            Router::new()
-                .route(
-                    "/:action_id",
-                    get(get::get_action.layer(permissions!["iam.action.get"])),
-                )
-                .route(
-                    "/",
-                    MethodRouter::new()
-                        .post(post::update_action.layer(permissions!["iam.action.update"]))
-                        .put(put::add_action.layer(permissions!["iam.action.add"]))
-                        .delete(delete::delete_action.layer(permissions!["iam.action.delete"])),
-                )
-        })
-        .nest("/actions", {
-            Router::new().route(
-                "/",
-                get(gets::list_actions).route_layer(permissions!["iam.action.list"]),
-            )
-        })
+        .route(
+            "/:action_id",
+            get(get::get_action.layer(permissions!["iam.action.get"])),
+        )
+        .route(
+            "/",
+            MethodRouter::new()
+                .get(gets::list_actions.layer(permissions!["iam.action.list"]))
+                .post(post::update_action.layer(permissions!["iam.action.update"]))
+                .put(put::add_action.layer(permissions!["iam.action.add"]))
+                .delete(delete::delete_action.layer(permissions!["iam.action.delete"])),
+        )
 }

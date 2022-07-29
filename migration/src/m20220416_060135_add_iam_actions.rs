0bb2203a4b4a970b03cf7821b9077e75c37654bb
Szepesi Tibor <sztibi023@gmail.com>
2022-07-29 02:56:53 +0200

    fix: migration action ids

--- migration/src/m20220416_060135_add_iam_actions.rs
+++ migration/src/m20220416_060135_add_iam_actions.rs
@@ -19,87 +19,87 @@ impl MigrationTrait for Migration {
                     .into_table(Entity)
                     .columns(vec![Column::Id, Column::Name, Column::Secure])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.action.add".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.action.get".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.action.update".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.action.list".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.action.delete".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.group.add".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.group.get".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.group.list".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.group.delete".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.group.edit".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.user.add".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.user.get".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.user.list".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.user.update".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.user.invite".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.user.delete".into(),
                         true.into(),
                     ])
                     .values_panic(vec![
-                        format!("ActionID-{}", create_action_id()).into(),
+                        create_action_id().into(),
                         "iam.policy.assign".into(),
                         true.into(),
                     ])

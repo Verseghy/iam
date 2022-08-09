0be79e5cdb78f3a88161ea90bdf12e4a255b27e4
Szepesi Tibor <sztibi023@gmail.com>
2022-08-09 19:13:24 +0200

    feat: global Error type

---
+++ iam/src/utils/error.rs
@@ -0,0 +1,82 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use lettre::transport::smtp::Error as SmtpError;
+use redis::RedisError;
+use sea_orm::DbErr;
+use serde_json::json;
+
+pub enum Error {
+    Internal(Box<dyn std::error::Error>),
+    NotFound(Option<String>),
+    BadRequest(Option<String>),
+    Unauthorized(Option<String>),
+}
+
+impl Error {
+    pub fn internal<E: Into<Box<dyn std::error::Error>>>(error: E) -> Self {
+        let error = error.into();
+        tracing::error!("internal error: {}", error);
+        Error::Internal(error)
+    }
+
+    pub fn not_found<S: AsRef<str>>(msg: S) -> Self {
+        Error::NotFound(Some(msg.as_ref().to_string()))
+    }
+
+    pub fn bad_request<S: AsRef<str>>(msg: S) -> Self {
+        Error::BadRequest(Some(msg.as_ref().to_string()))
+    }
+
+    pub fn unauthorized<S: AsRef<str>>(msg: S) -> Self {
+        Error::Unauthorized(Some(msg.as_ref().to_string()))
+    }
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        fn response_with_msg<S: AsRef<str>>(status: StatusCode, msg: Option<S>) -> Response {
+            if let Some(msg) = msg {
+                (
+                    status,
+                    Json(json!({
+                        "error": msg.as_ref(),
+                    })),
+                )
+                    .into_response()
+            } else {
+                status.into_response()
+            }
+        }
+
+        match self {
+            Self::Internal(_) => response_with_msg(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Some("internal server error"),
+            ),
+            Self::NotFound(msg) => response_with_msg(StatusCode::NOT_FOUND, msg),
+            Self::BadRequest(msg) => response_with_msg(StatusCode::BAD_REQUEST, msg),
+            Self::Unauthorized(msg) => response_with_msg(StatusCode::UNAUTHORIZED, msg),
+        }
+    }
+}
+
+impl From<DbErr> for Error {
+    fn from(error: DbErr) -> Self {
+        Error::internal(error)
+    }
+}
+
+impl From<RedisError> for Error {
+    fn from(error: RedisError) -> Self {
+        Error::internal(error)
+    }
+}
+
+impl From<SmtpError> for Error {
+    fn from(error: SmtpError) -> Self {
+        Error::internal(error)
+    }
+}

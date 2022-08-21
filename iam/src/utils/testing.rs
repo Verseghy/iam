04a1bacbc688f2b5de45bb15a160b3a822ee1fea
Szepesi Tibor <sztibi023@gmail.com>
2022-08-21 16:40:42 +0200

    feat: json_body testing utility

--- iam/src/utils/testing.rs
+++ iam/src/utils/testing.rs
@@ -9,3 +9,15 @@ where
     let bytes = hyper::body::to_bytes(body).await.unwrap();
     serde_json::from_slice(&bytes).unwrap()
 }
+
+macro_rules! json_body {
+    ($($tt:tt)*) => {
+        ::axum::body::Body::from(
+            ::serde_json::to_vec(
+                &::serde_json::json!($($tt)*)
+            ).unwrap(),
+        )
+    }
+}
+
+pub(crate) use json_body;

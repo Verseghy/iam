3ea6c0ac67b4566d2194eab03f426bee6a9af5ea
Szepesi Tibor <sztibi023@gmail.com>
2022-08-22 19:24:26 +0200

    chore: remove lettre and redis

--- iam/src/shared.rs
+++ iam/src/shared.rs
@@ -1,34 +1,21 @@
 use crate::token::Jwt;
 use common::database;
-use lettre::{
-    transport::smtp::{authentication::Credentials, Error as SmtpError},
-    AsyncSmtpTransport, Tokio1Executor,
-};
 use rand::{rngs::StdRng, SeedableRng};
-use redis::aio::ConnectionManager;
 use sea_orm::DbConn;
 use std::sync::Arc;
 
-pub type SmtpTransport = AsyncSmtpTransport<Tokio1Executor>;
-
 pub trait SharedTrait: Clone + Send + Sync + 'static {
     type Db: sea_orm::ConnectionTrait;
-    type Redis: redis::AsyncCommands + Clone;
-    type Smtp: lettre::AsyncTransport<Error = SmtpError> + Sync;
     type Jwt: crate::token::JwtTrait;
     type Rng: rand::Rng + Clone;
 
     fn db(&self) -> &Self::Db;
-    fn redis(&self) -> &Self::Redis;
-    fn smtp(&self) -> &Self::Smtp;
     fn jwt(&self) -> &Self::Jwt;
     fn rng(&self) -> &Self::Rng;
 }
 
 pub struct SharedInner {
     pub db: DbConn,
-    pub redis: ConnectionManager,
-    pub smtp: SmtpTransport,
     pub jwt: Jwt,
     pub rng: StdRng,
 }
@@ -40,21 +27,11 @@ pub struct Shared {
 
 impl SharedTrait for Shared {
     type Db = DbConn;
-    type Redis = ConnectionManager;
-    type Smtp = SmtpTransport;
     type Jwt = Jwt;
     type Rng = StdRng;
 
     fn db(&self) -> &DbConn {
         &self.inner.db
-    }
-
-    fn redis(&self) -> &ConnectionManager {
-        &self.inner.redis
-    }
-
-    fn smtp(&self) -> &SmtpTransport {
-        &self.inner.smtp
     }
 
     fn jwt(&self) -> &Jwt {
@@ -70,24 +47,10 @@ pub async fn create_shared() -> Shared {
     Shared {
         inner: Arc::new(SharedInner {
             db: database::connect().await,
-            redis: database::connect_redis().await,
-            smtp: create_smtp_transport(),
             jwt: Jwt::new(),
             rng: StdRng::from_entropy(),
         }),
     }
-}
-
-fn create_smtp_transport() -> SmtpTransport {
-    AsyncSmtpTransport::<Tokio1Executor>::starttls_relay(
-        &std::env::var("SMTP_HOST").expect("SMTP_HOST not set"),
-    )
-    .unwrap()
-    .credentials(Credentials::new(
-        std::env::var("SMTP_USERNAME").expect("SMTP_USERNAME not set"),
-        std::env::var("SMTP_PASSWORD").expect("SMTP_PASSWORD not set"),
-    ))
-    .build()
 }
 
 #[cfg(test)]
@@ -95,14 +58,11 @@ pub mod mock {
     #![allow(unused)]
 
     use super::*;
-    use crate::mock::{MockRedis, MockSmtpTransport};
     use rand::rngs::mock::StepRng;
     use sea_orm::MockDatabase;
 
     pub struct MockSharedInner {
         db: Option<DbConn>,
-        redis: Option<MockRedis>,
-        smtp: Option<MockSmtpTransport>,
         jwt: Option<Jwt>,
         rng: Option<StepRng>,
     }
@@ -116,8 +76,6 @@ pub mod mock {
         pub fn builder() -> MockSharedInner {
             MockSharedInner {
                 db: None,
-                redis: None,
-                smtp: None,
                 jwt: None,
                 rng: None,
             }
@@ -131,16 +89,6 @@ pub mod mock {
     impl MockSharedInner {
         pub fn db(mut self, db: MockDatabase) -> Self {
             self.db = Some(db.into_connection());
-            self
-        }
-
-        pub fn redis(mut self, redis: MockRedis) -> Self {
-            self.redis = Some(redis);
-            self
-        }
-
-        pub fn smtp(mut self, smtp: MockSmtpTransport) -> Self {
-            self.smtp = Some(smtp);
             self
         }
 
@@ -163,21 +111,11 @@ pub mod mock {
 
     impl SharedTrait for MockShared {
         type Db = DbConn;
-        type Redis = MockRedis;
-        type Smtp = MockSmtpTransport;
         type Jwt = Jwt;
         type Rng = StepRng;
 
         fn db(&self) -> &DbConn {
             self.inner.db.as_ref().expect("database not set")
-        }
-
-        fn redis(&self) -> &MockRedis {
-            self.inner.redis.as_ref().expect("redis not set")
-        }
-
-        fn smtp(&self) -> &MockSmtpTransport {
-            self.inner.smtp.as_ref().expect("smtp not set")
         }
 
         fn jwt(&self) -> &Jwt {

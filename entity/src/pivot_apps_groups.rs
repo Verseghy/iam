0855555031c4b623e9ee8360c8bff6fc3198c4df
Szepesi Tibor <sztibi023@gmail.com>
2022-10-25 11:31:23 +0200

    fix: s

--- entity/src/pivot_apps_groups.rs
+++ entity/src/pivot_apps_groups.rs
@@ -1,7 +1,7 @@
 use sea_orm::entity::prelude::*;
 
 #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
-#[sea_orm(table_name = "pivot_app_groups")]
+#[sea_orm(table_name = "pivot_apps_groups")]
 pub struct Model {
     #[sea_orm(primary_key)]
     pub app_id: String,

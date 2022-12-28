6c4afa7f0f27391601cbb33e94ab3bd0552221f4
Szepesi Tibor <sztibi023@gmail.com>
2022-12-28 02:21:50 +0100

    chore(deps): upgrade

--- iam/src/json.rs
+++ iam/src/json.rs
@@ -2,7 +2,8 @@ use crate::utils::Error;
 use axum::{
     async_trait,
     body::HttpBody,
-    extract::{rejection::JsonRejection, FromRequest, RequestParts},
+    extract::{rejection::JsonRejection, FromRequest},
+    http::Request,
     response::{IntoResponse, Response},
     BoxError,
 };
@@ -12,17 +13,18 @@ use validator::Validate;
 pub struct Json<T>(pub T);
 
 #[async_trait]
-impl<B, T> FromRequest<B> for Json<T>
+impl<T, S, B> FromRequest<S, B> for Json<T>
 where
     T: DeserializeOwned,
-    B: HttpBody + Send,
+    B: HttpBody + Send + 'static,
     B::Data: Send,
     B::Error: Into<BoxError>,
+    S: Send + Sync,
 {
     type Rejection = Error;
 
-    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
-        match axum::Json::<T>::from_request(req).await {
+    async fn from_request(req: Request<B>, state: &S) -> Result<Self, Self::Rejection> {
+        match axum::Json::<T>::from_request(req, state).await {
             Ok(value) => Ok(Self(value.0)),
             Err(rejection) => match rejection {
                 JsonRejection::JsonDataError(_) => Err(Error::bad_request("missing fields")),
@@ -48,17 +50,18 @@ where
 pub struct ValidatedJson<T>(pub T);
 
 #[async_trait]
-impl<T, B> FromRequest<B> for ValidatedJson<T>
+impl<T, S, B> FromRequest<S, B> for ValidatedJson<T>
 where
     T: DeserializeOwned + Validate,
-    B: HttpBody + Send,
+    B: HttpBody + Send + 'static,
     B::Data: Send,
     B::Error: Into<BoxError>,
+    S: Send + Sync,
 {
     type Rejection = Error;
 
-    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
-        let Json(json) = req.extract::<Json<T>>().await?;
+    async fn from_request(req: Request<B>, state: &S) -> Result<Self, Self::Rejection> {
+        let Json(json) = Json::<T>::from_request(req, state).await?;
 
         json.validate()
             .map_err(|_| Error::bad_request("invalid data"))?;

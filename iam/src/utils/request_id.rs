97c23710850b2b4792ffe0d3fec24d98cf6468fd
Szepesi Tibor <sztibi023@gmail.com>
2023-01-13 17:28:31 +0100

    refactor: use constant in uuid lib instead

--- iam/src/utils/request_id.rs
+++ iam/src/utils/request_id.rs
@@ -1,13 +1,13 @@
 use hyper::{header::HeaderValue, Request};
 use tower_http::request_id::{MakeRequestId, RequestId};
-use uuid::Uuid;
+use uuid::{fmt::Simple, Uuid};
 
 #[derive(Copy, Clone, Debug)]
 pub struct MakeUuidRequestId;
 
 impl MakeRequestId for MakeUuidRequestId {
     fn make_request_id<B>(&mut self, _request: &Request<B>) -> Option<RequestId> {
-        let mut buf = [0u8; 32];
+        let mut buf = [0u8; Simple::LENGTH];
         let id = Uuid::new_v4().as_simple().encode_lower(&mut buf);
         Some(RequestId::new(HeaderValue::from_str(id).unwrap()))
     }

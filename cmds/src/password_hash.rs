65c0c106cc49ee718bcb2a5d6b5ebddc0ae3c01c
Szepesi Tibor <sztibi023@gmail.com>
2023-02-09 02:35:43 +0100

    fix: clippy

--- cmds/src/password_hash.rs
+++ cmds/src/password_hash.rs
@@ -1,5 +1,5 @@
 fn main() {
     let input = std::env::args().nth(1).expect("no input");
     let hashed = common::password::hash(&input).expect("failed to hash");
-    println!("{}", hashed);
+    println!("{hashed}");
 }

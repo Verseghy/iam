dde34549adf51f3fdadeb84e973054ad5b4aeb5b
Szepesi Tibor <sztibi023@gmail.com>
2023-02-26 22:29:40 +0100

    feat: list apps

--- iam/src/handlers/v1/apps/mod.rs
+++ iam/src/handlers/v1/apps/mod.rs
@@ -1,14 +1,22 @@
 mod create;
+mod list;
 mod login;
 
 use crate::{auth::permissions, shared::SharedTrait};
-use axum::{routing::post, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 
 pub fn routes<S: SharedTrait>() -> Router {
     Router::new()
         .route(
             "/create",
-            post(create::create_app::<S>).layer(permissions![S, "iam.app.create"]),
+            post(create::create_app::<S>).layer(permissions![S, "iam.apps.create"]),
         )
         .route("/login", post(login::login_app::<S>))
+        .route(
+            "/",
+            get(list::list_apps::<S>).layer(permissions![S, "iam.apps.list"]),
+        )
 }

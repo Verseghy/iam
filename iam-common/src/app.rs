37affa072e944ea9c822c6f1649dec49b95f7b75
Szepesi Tibor <sztibi023@gmail.com>
2023-03-01 09:23:21 +0100

    refactor: move app creation to iam-common

--- iam-common/src/app.rs
+++ iam-common/src/app.rs
@@ -1,5 +1,11 @@
-use crate::error::{self, Result};
+use crate::{
+    error::{self, Result},
+    Id,
+};
 use base64::{engine::general_purpose::STANDARD, Engine};
+use iam_entity::apps;
+use rand::distributions::{Alphanumeric, DistString};
+use sea_orm::{ConnectionTrait, EntityTrait, Set};
 
 pub fn parse_token(token: &str) -> Result<(String, String)> {
     let decoded = STANDARD
@@ -12,4 +18,30 @@ pub fn parse_token(token: &str) -> Result<(String, String)> {
         .ok_or(error::APP_INVALID_TOKEN)?;
 
     Ok((id.to_owned(), password.to_owned()))
+}
+
+pub async fn create<D>(db: &D, name: &str) -> Result<(Id, String)>
+where
+    D: ConnectionTrait,
+{
+    let id = Id::new_app();
+
+    let password = {
+        let mut rng = rand::thread_rng();
+        Alphanumeric.sample_string(&mut rng, 32)
+    };
+
+    let hashed_password = crate::password::hash(&password)?;
+
+    let app = apps::ActiveModel {
+        id: Set(id.to_string()),
+        name: Set(name.to_owned()),
+        password: Set(hashed_password),
+        ..Default::default()
+    };
+
+    apps::Entity::insert(app).exec(db).await?;
+
+    let secret = STANDARD.encode(format!("{}:{}", id.to_string(), password));
+    Ok((id, secret))
 }

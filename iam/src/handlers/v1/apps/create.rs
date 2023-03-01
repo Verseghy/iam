c5e786c137b1d88e4898d1d0b5e9575cbb46ffcd
Szepesi Tibor <sztibi023@gmail.com>
2023-03-01 09:23:21 +0100

    refactor: move app creation to iam-common

--- iam/src/handlers/v1/apps/create.rs
+++ iam/src/handlers/v1/apps/create.rs
@@ -4,9 +4,6 @@ use crate::{
 };
 use axum::{http::StatusCode, Extension};
 use iam_common::{error::Result, Id};
-use iam_entity::apps;
-use rand::distributions::{Alphanumeric, DistString};
-use sea_orm::{EntityTrait, Set};
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
@@ -26,21 +23,7 @@ pub async fn create_app<S: SharedTrait>(
     Extension(shared): Extension<S>,
     ValidatedJson(req): ValidatedJson<Request>,
 ) -> Result<(StatusCode, Json<Response>)> {
-    let id = Id::new_app();
-    let password = Alphanumeric.sample_string(&mut shared.rng().clone(), 32);
-
-    let hashed_password = iam_common::password::hash(&password)?;
-
-    let app = apps::ActiveModel {
-        id: Set(id.to_string()),
-        name: Set(req.name),
-        password: Set(hashed_password),
-        ..Default::default()
-    };
-
-    apps::Entity::insert(app).exec(shared.db()).await?;
-
-    let secret = base64::encode(format!("{}:{}", id.to_string(), password));
+    let (id, secret) = iam_common::app::create(shared.db(), &req.name).await?;
 
     Ok((StatusCode::CREATED, Json(Response { id, secret })))
 }

ddff6fd371548fa4e9d38b93dee976ff3bb9f41f
Szepesi Tibor <sztibi023@gmail.com>
2023-03-02 10:11:46 +0100

    style: format

--- iam-common/src/app.rs
+++ iam-common/src/app.rs
@@ -8,25 +8,20 @@ use rand::distributions::{Alphanumeric, DistString};
 use sea_orm::{ConnectionTrait, EntityTrait, Set};
 
 pub fn parse_token(token: &str) -> Result<(String, String)> {
-    let decoded = BASE64_STANDARD_NO_PAD
-        .decode(token)
-        .map_err(|err| {
-            tracing::warn!("token is not valid base64: {err}, {token}");
-            error::APP_INVALID_TOKEN
-        })?;
+    let decoded = BASE64_STANDARD_NO_PAD.decode(token).map_err(|err| {
+        tracing::warn!("token is not valid base64: {err}, {token}");
+        error::APP_INVALID_TOKEN
+    })?;
 
-    let decoded_string = String::from_utf8(decoded)
-        .map_err(|_| {
-            tracing::warn!("decoded token is not valid utf8");
-            error::APP_INVALID_TOKEN
-        })?;
+    let decoded_string = String::from_utf8(decoded).map_err(|_| {
+        tracing::warn!("decoded token is not valid utf8");
+        error::APP_INVALID_TOKEN
+    })?;
 
-    let (id, password) = decoded_string
-        .split_once(':')
-        .ok_or_else(|| {
-            tracing::warn!("token doesn't contain a ':'");
-            error::APP_INVALID_TOKEN
-        })?;
+    let (id, password) = decoded_string.split_once(':').ok_or_else(|| {
+        tracing::warn!("token doesn't contain a ':'");
+        error::APP_INVALID_TOKEN
+    })?;
 
     Ok((id.to_owned(), password.to_owned()))
 }

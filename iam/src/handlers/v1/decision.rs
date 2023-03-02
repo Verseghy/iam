ae6fae40dcf667c3c5fa67e0c98361bf452dae8a
Szepesi Tibor <sztibi023@gmail.com>
2023-03-02 10:17:37 +0100

    fix: clippy

--- iam/src/handlers/v1/decision.rs
+++ iam/src/handlers/v1/decision.rs
@@ -1,9 +1,7 @@
 use crate::{auth, json::Json, shared::SharedTrait};
 use axum::{http::StatusCode, Extension};
-use iam_common::{error::Result, token::Claims, Id};
+use iam_common::{error::Result, Id};
 use serde::Deserialize;
-use std::sync::Arc;
-use tracing::instrument;
 
 #[derive(Deserialize, Debug)]
 pub struct Request {
@@ -13,7 +11,6 @@ pub struct Request {
 
 pub async fn decision<S: SharedTrait>(
     Extension(shared): Extension<S>,
-    Extension(claims): Extension<Arc<Claims>>,
     Json(req): Json<Request>,
 ) -> Result<StatusCode> {
     let actions: Vec<&str> = req.actions.iter().map(|x| x.as_str()).collect();

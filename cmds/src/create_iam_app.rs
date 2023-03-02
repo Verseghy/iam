c32f2690df8c8c77c8c5fe6ac7f61dea57d85f80
Szepesi Tibor <sztibi023@gmail.com>
2023-03-02 17:26:59 +0100

    feat: create_iam_app command

---
+++ cmds/src/create_iam_app.rs
@@ -0,0 +1,19 @@
+use std::env::{self, args};
+
+use dotenvy::dotenv;
+use libiam::testing::{apps::create_app, Database};
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let name = args().nth(1).expect("no name given");
+
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set");
+    let database = Database::connect(&db_url).await;
+
+    let (id, secret) = create_app(&database, &name).await;
+
+    println!("id: {}", id.to_string());
+    println!("secret: {}", secret);
+}

666e71c898e450ea3fd17a52d793f37734d56e41
Zoltán Szepesi <2dcube01@gmail.com>
2023-11-27 19:39:03 +0100

    Always return token and ID on libiam users (#68)

--- libiam/src/user.rs
+++ libiam/src/user.rs
@@ -3,7 +3,9 @@ use crate::{
     utils::Either,
     Iam,
 };
+use iam_common::token::Claims;
 use iam_common::Id;
+use jsonwebtoken::{Algorithm, DecodingKey, Validation};
 use reqwest::Client;
 use serde::Deserialize;
 use serde_json::json;
@@ -12,6 +14,7 @@ use std::sync::Arc;
 #[derive(Debug)]
 pub struct UserInner {
     token: String,
+    id: Id,
     _iam: Iam,
 }
 
@@ -21,14 +24,8 @@ pub struct User {
 }
 
 impl User {
-    pub async fn register(iam: &Iam, name: &str, email: &str, password: &str) -> Result<Id> {
-        #[derive(Debug, Deserialize)]
-        #[allow(unused)]
-        struct Response {
-            id: Id,
-        }
-
-        let res = Client::new()
+    pub async fn register(iam: &Iam, name: &str, email: &str, password: &str) -> Result<Self> {
+        Client::new()
             .post(iam.get_url("/v1/users/register"))
             .json(&json!({
                 "name": name,
@@ -36,13 +33,9 @@ impl User {
                 "password": password,
             }))
             .send()
-            .await?
-            .json::<Either<Response, ErrorMessage>>()
             .await?;
 
-        let res = unwrap_res(res)?;
-
-        Ok(res.id)
+        User::login(iam, email, password).await
     }
 
     pub async fn login(iam: &Iam, email: &str, password: &str) -> Result<Self> {
@@ -68,7 +61,29 @@ impl User {
 
         Ok(Self {
             inner: Arc::new(UserInner {
-                token: res.token,
+                token: res.token.clone(),
+                id: serde_json::from_str::<Id>(
+                    format!(
+                        // HACK: impl FromStr
+                        "\"{}\"",
+                        jsonwebtoken::decode::<Claims>(
+                            res.token.as_str(),
+                            &DecodingKey::from_secret(&[]),
+                            &{
+                                let mut v = Validation::new(Algorithm::RS256);
+                                v.insecure_disable_signature_validation();
+                                v.set_audience(&["https://verseghy-gimnazium.net"]);
+                                v
+                            },
+                        )
+                        .unwrap()
+                        .claims
+                        .subject
+                        .as_str()
+                    )
+                    .as_str(),
+                )
+                .unwrap(),
                 _iam: iam.clone(),
             }),
         })
@@ -76,5 +91,9 @@ impl User {
 
     pub fn token(&self) -> &str {
         &self.inner.token
+    }
+
+    pub fn id(&self) -> &Id {
+        &self.inner.id
     }
 }

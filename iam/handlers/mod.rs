9f5327622846f8f9984f4da99a5c1c315bce8792
Szepesi Tibor <smrtrfszm@proton.me>
2024-11-17 17:10:35 +0100

    Add basic liveness and readiness probe

--- iam/handlers/mod.rs
+++ iam/handlers/mod.rs
@@ -3,11 +3,13 @@ mod v1;
 mod well_known;
 
 use crate::shared::SharedTrait;
-use axum::Router;
+use axum::{routing::get, Router};
 
 pub fn routes<S: SharedTrait>() -> Router {
     Router::new()
         .nest("/v1", v1::routes::<S>())
         .nest("/internal", internal::routes::<S>())
         .nest("/.well-known", well_known::routes::<S>())
+        .route("/ready", get(|| async {}))
+        .route("/live", get(|| async {}))
 }

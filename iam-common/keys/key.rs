4633ca3c28a57619d5ef451a36f3d60bc2d79f8c
Szepesi Tibor <smrtrfszm@proton.me>
2024-12-08 02:46:13 +0100

    Use jwt secret from env var

--- iam-common/keys/key.rs
+++ iam-common/keys/key.rs
@@ -1,4 +1,6 @@
-use ed25519_dalek::{pkcs8::EncodePrivateKey, SigningKey};
+use std::env;
+use base64::{prelude::BASE64_STANDARD, Engine};
+use ed25519_dalek::{pkcs8::EncodePrivateKey, SecretKey, SigningKey, SECRET_KEY_LENGTH};
 use jose_jwk::{Jwk, Okp, OkpCurves, Parameters};
 use jsonwebtoken::{Algorithm, DecodingKey, EncodingKey};
 use rand::rngs::OsRng;
@@ -10,8 +12,14 @@ pub struct Key {
 }
 
 impl Key {
-    pub(super) fn generate() -> Key {
+    #[allow(unused)]
+    pub(super) fn generate() -> Self {
         let private_key = SigningKey::generate(&mut OsRng);
+        Self::from_private_key(private_key.as_bytes())
+    }
+
+    pub(super) fn from_private_key(secret_key: &SecretKey) -> Self {
+        let private_key = SigningKey::from_bytes(secret_key);
         let public_key = private_key.verifying_key();
 
         let bytes = Box::new(public_key.to_bytes()) as Box<[u8]>;
@@ -36,6 +44,14 @@ impl Key {
             encoding,
             decoding,
         }
+    }
+
+    // TODO: this is a temporary solution
+    pub(super) fn from_env() -> Self {
+        let key = env::var("IAM_JWT_SECRET_KEY").unwrap();
+        let key = BASE64_STANDARD.decode(key).unwrap();
+        assert_eq!(key.len(), SECRET_KEY_LENGTH);
+        Self::from_private_key(&key.try_into().unwrap())
     }
 
     pub fn get_alg(&self) -> Algorithm {

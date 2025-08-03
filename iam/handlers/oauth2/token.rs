92a510e963ca8ab470047379cc7557e949ddbceb
Szepesi Tibor <smrtrfszm@proton.me>
2025-08-04 00:18:45 +0200

    Implement Resource Owner Password Credentials Grant (#108)

---
+++ iam/handlers/oauth2/token.rs
@@ -0,0 +1,105 @@
+use crate::{json::Json, shared::SharedTrait};
+use axum::{
+    body::Bytes,
+    response::{IntoResponse, Response},
+    Extension,
+};
+use iam_common::{
+    error::{oauth::OAuthError, Error},
+    keys::jwt::Claims,
+    password,
+};
+use iam_entity::users;
+use sea_orm::{ActiveModelTrait, ActiveValue, ColumnTrait, EntityTrait, QueryFilter};
+use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
+
+#[derive(Debug, Serialize)]
+pub struct TokenResponse {
+    access_token: String,
+    token_type: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    expires_in: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    refresh_token: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    scope: Option<String>,
+}
+
+pub async fn token<S: SharedTrait>(
+    Extension(shared): Extension<S>,
+    body: Bytes,
+) -> Result<Json<TokenResponse>, Response> {
+    #[derive(Deserialize)]
+    pub struct GrantTypeRequest<'a> {
+        #[serde(borrow)]
+        grant_type: Cow<'a, str>,
+    }
+
+    let request = serde_urlencoded::from_bytes::<GrantTypeRequest<'_>>(&body)
+        .map_err(|_| OAuthError::invalid_request().into_response())?;
+
+    if &request.grant_type == "password" {
+        let request =
+            serde_urlencoded::from_bytes::<ResourceOwnerPasswordCredentialsGrantRequest<'_>>(&body)
+                .map_err(|_| OAuthError::invalid_request().into_response())?;
+
+        return resource_owner_password_credentials_grant(&shared, request).await;
+    }
+
+    Err(OAuthError::unsupported_grant_type().into_response())
+}
+
+#[derive(Debug, Deserialize)]
+struct ResourceOwnerPasswordCredentialsGrantRequest<'a> {
+    #[serde(borrow)]
+    username: Cow<'a, str>,
+    #[serde(borrow)]
+    password: Cow<'a, str>,
+}
+
+async fn resource_owner_password_credentials_grant<S: SharedTrait>(
+    shared: &S,
+    request: ResourceOwnerPasswordCredentialsGrantRequest<'_>,
+) -> Result<Json<TokenResponse>, Response> {
+    let res = users::Entity::find()
+        .filter(users::Column::Email.eq(request.username.as_ref()))
+        .one(shared.db())
+        .await
+        .map_err(|err| Error::from(err).into_response())?;
+
+    let Some(res) = res else {
+        // Still compute hash if the user is not in the database to avoid timing attacks
+        _ = password::hash(&request.password);
+        return Err(OAuthError::invalid_grant().into_response());
+    };
+
+    let (valid, rehash) = password::validate(&res.password, request.password.as_ref())
+        .map_err(|_| OAuthError::invalid_grant().into_response())?;
+
+    if let Some(Ok(hash)) = rehash {
+        let mut action: users::ActiveModel = res.clone().into();
+        action.password = ActiveValue::Set(hash);
+
+        action
+            .update(shared.db())
+            .await
+            .map_err(|err| Error::from(err).into_response())?;
+    }
+
+    if !valid {
+        return Err(OAuthError::invalid_grant().into_response());
+    }
+
+    crate::audit!(action = "login", user = res.id);
+
+    let token = shared.key_manager().jwt().encode(&Claims::new(res.id));
+
+    Ok(Json(TokenResponse {
+        access_token: token,
+        token_type: String::from("bearer"),
+        refresh_token: None,
+        expires_in: None,
+        scope: None,
+    }))
+}
